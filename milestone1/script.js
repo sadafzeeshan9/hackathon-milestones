c0c79c2fc2f493396277c3f435600809a14da7e5
sadafzeeshan9 <160055415+sadafzeeshan9@users.noreply.github.com>
2024-11-27 16:00:51 +0500

    Add files via upload

---
+++ milestone1/script.js
@@ -0,0 +1,13 @@
+var button = document.getElementById('button');
+var skills = document.getElementById('skills');
+button.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
+var button1 = document.getElementById('button1');
+button1.addEventListener('click', function () {
+});
---
+++ milestone1/script.ts
@@ -0,0 +1,17 @@
+const button = document.getElementById('button') as HTMLButtonElement
+
+const skills = document.getElementById('skills') as HTMLElement
+
+button.addEventListener('click', ()=> {
+    if (skills.style.display === 'none'){
+        skills.style.display = 'block'
+    }
+else{
+    skills.style.display = 'none'
+}
+
+ });
+
+ 
+
+ 
